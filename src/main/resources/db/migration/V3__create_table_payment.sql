c36464f56acd426eee80274d9b89ebad1184c14b
s7985_Shevtsov <s7985@atins.pl>
2025-05-20 12:58:06 +0300

    implenent payment

---
+++ src/main/resources/db/migration/V3__create_table_payment.sql
@@ -0,0 +1,14 @@
+create table if not exists payment
+(
+    id               bigserial   not null,
+    amount           decimal(10,2) not null,
+    payment_date     datetime not null,
+    payment_method   varchar(30) not null,
+    reference_code   varchar(50) not null,
+    status           varchar(20) not null,
+    title            varchar(100) not null,
+    created_at timestamp   not null default now(),
+    updated_at timestamp   not null default now(),
+    constraint pk_payment primary key (id),
+    constraint uq_payment unique (reference_code)
+);
