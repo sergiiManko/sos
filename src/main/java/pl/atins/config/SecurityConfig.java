72d0de3af055c3c8b3a56acc278a42d3ed6e8d13
Serhii Manko <s7810@atins.pl>
2025-06-02 11:17:30 +0200

    Remove unnecessary configuration for Spring Security

--- src/main/java/pl/atins/config/SecurityConfig.java
+++ src/main/java/pl/atins/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package pl.atins.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,7 +11,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import pl.atins.service.AdminDetailsService;
 
 /**
  * @author Serhii Manko
@@ -21,17 +19,11 @@ import pl.atins.service.AdminDetailsService;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    private final AdminDetailsService adminDetailsService;
-
-    public SecurityConfig(AdminDetailsService adminDetailsService) {
-        this.adminDetailsService = adminDetailsService;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authenticationProvider(authenticationProvider())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/login/error", "/register", "/forgetPassword").permitAll()
                         .requestMatchers("/css/**", "/js/**", "/images/**", "/icons/**", "/fonts/**", "/vendor/**", "/adminlte/**").permitAll()
@@ -73,13 +65,5 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(adminDetailsService);
-        authProvider.setPasswordEncoder(getPasswordEncoder());
-        return authProvider;
     }
 }
